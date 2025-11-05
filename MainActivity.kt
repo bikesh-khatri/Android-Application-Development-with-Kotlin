31e4b1fddebe16c5c785d41af8414768ede8b72d
Bikesh Khatri <khatr@MacBook-Air.local>
2025-11-05 10:24:03 +0545

    project

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstkotlin.ui.theme.FirstKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +34,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstKotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                  MainBody()
             }
         }
     }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainBody(){
+    Column(
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstKotlinTheme {
-        Greeting("Android")
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color= colorResource(R.color.white))
+    ){
+        Text("Hello",
+            modifier = Modifier.fillMaxWidth(),
+            style= TextStyle(
+                fontSize = 40.sp,
+                textDecoration = TextDecoration.Underline,
+                color = Color.Yellow,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ))
+        Text("world")
+        Image(
+            painter = painterResource(R.drawable.tiger),
+            contentDescription = null,
+            modifier= Modifier.size(200.dp)
+                .align(Alignment.CenterHorizontally)
+
+        )
     }
+}
+@Preview
+@Composable
+fun MainPreview(){
+    MainBody()
 }
---
+++ ProfileActivity.kt
@@ -0,0 +1,45 @@
+package com.example.firstkotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstkotlin.ui.theme.FirstKotlinTheme
+
+class ProfileActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProfileBody()
+
+        }
+    }
+}
+@Composable
+fun ProfileBody(){
+    Scaffold { padding ->
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)){ }
+    }
+}
+@Composable
+@Preview
+fun ProfilePreview(){
+    ProfileBody()
+}
+
